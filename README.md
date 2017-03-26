29d634934604e217fd3cf98b81c8349eaa45111b
Gregor Srdic <gregor.srdic@gmail.com>
2017-03-26 13:07:44 +0200

    updated bottom sheet readme

--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 
 ## Accordion Component Group
 
-*Component combining multiple accordion components. Opening one of the accordions closes other any other open accordion in the group.*
+*Component wrapping multiple accordion components. Opening one of the accordions closes other any other open accordion in the group.*
 
 [Source](https://github.com/gregor-srdic/material-start/blob/master/app/src/accordion/accordion-group-component.js)
 
@@ -36,31 +36,14 @@
 </md-accordion-group>
 ```
 
-## Accordion directive
-
-*Single accordion element, implemented as md-accordion added to md-card. Initial state can be set througt true/false value of md-accordion attribute.*
-
-[Source](https://github.com/gregor-srdic/material-start/blob/master/app/src/accordion/accordion-directive.js)
-
-
-```
-<md-card md-accordion="true">
-  <md-card-header md-ink-ripple>
-    <md-card-header-text><span class="md-title">Accordion header</span></md-card-header-text>
-    <md-icon class="expand-icon" md-svg-icon="arrow_down"></md-icon>
-  </md-card-header>
-  <md-card-content>
-    Accordion content
-  </md-card-content>
-</md-card>
-```
-
 
 # Bottom sheet
 
 [Live example](http://pepa.azurewebsites.net/app/?q=bottom)
 
 [Usage example](https://github.com/gregor-srdic/material-start/blob/master/app/src/bottom-sheets-demo/bottom-sheets-demo.html)
+
+[Source](https://github.com/gregor-srdic/material-start/blob/master/app/src/accordion/accordion-group-component.js)
 
 
 
--- app/src/bottom-sheets/bottom-sheets-scrollable-directive.js
+++ app/src/bottom-sheets/bottom-sheets-scrollable-directive.js
@@ -1,10 +1,10 @@
-function BottomSheetScrollableDirective($timeout) {
+function BottomSheetScrollableDirective($timeout, $window) {
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
 
             scope.addScrollableContentFix = () => {
-                // 88px is padding for md-bottom-sheet
+                // Need to take into account 88px padding-bottom of md-bottom-sheet element
                 let h = element[0].offsetHeight - 88,
                     h1 = scope.bottomSheetContent[0].offsetHeight;
                 // md-content height is bigger than md-bottom-sheet heith, we have reached height limit, content max height can now be limited to thax px amount
@@ -19,31 +19,28 @@ function BottomSheetScrollableDirective($timeout) {
                 evt.stopPropagation();
             });
 
-            // apply scrollable content fix with slight delay to ensure measured element heights are correct
-            //$timeout(scope.addScrollableContentFix, 300);
-
-            // Watch md-content-height for changes. 
-            // Neccessary if elements are dynamically added to the bottom sheet and md-content might grow larger than md-bottom-sheet later on
+            // Scroll fix is applied when height of the md-content element changes. 
+            // Also neccessary to watch for changes. If elements are dynamically added to the bottom sheet and md-content might grow larger than md-bottom-sheet later on.
             scope.$watch(
                 () => scope.bottomSheetContent[0].offsetHeight,
                 (newValue, oldValue) => {
-                    //console.log(scope.bottomSheetContent[0].scrollHeight);
                     if (newValue != oldValue)
                         scope.addScrollableContentFix();
                 }
             );
-            /*
-            scope.$watch(
-                () => element[0].offsetHeight,
-                (newValue, oldValue) => {
-                    console.log(2);
-                    if (newValue != oldValue)
-                        scope.addScrollableContentFix();
-                }
-            );
-            */
+
+            // Taking care of fixed max-height for md-content and relative max-height for md-bottom-sheet, when window size changes
+            let onWindowResize = () => {
+                // This will allow container and content to fully stretch, and then apply 
+                scope.bottomSheetContent.css({ maxHeight: '' });
+                $timeout(scope.addScrollableContentFix, 300);
+            };
+            scope.$on('$destroy', function () {
+                angular.element($window).off('resize', onWindowResize);
+            });
+            angular.element($window).on('resize', onWindowResize);
         },
         scope: {}
     };
 }
-export default ['$timeout', BottomSheetScrollableDirective];
+export default ['$timeout', '$window', BottomSheetScrollableDirective];
